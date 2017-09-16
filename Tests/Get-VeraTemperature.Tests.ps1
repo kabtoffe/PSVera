912a7b2e28babb3d7c205aaaa269f7debe284a52
Toffe Bergström <toffe@kabtoffe.net>
2017-09-16 20:38:46 +0300

    add test for Get-Temperature

---
+++ Tests/Get-VeraTemperature.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$ModuleDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$testdir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$ModuleDir = (Get-Item "$ModuleDir\..\PSVera").FullName
+
+Import-Module "$ModuleDir\PSVera.psm1" -Force
+
+Describe "Test-VeraService" {
+
+    Mock Invoke-VeraAPI -ModuleName PSVera
+
+    It "Gets temperature" {
+        Get-VeraTemperature -DeviceNum 4
+
+        Assert-MockCalled -CommandName Invoke-VeraAPI -Scope It -ModuleName PSVera -ParameterFilter {
+            $DeviceNum -and $DeviceNum -eq 4 -and
+            $Id -eq "variableget" -and
+            $ServiceId -eq "urn:upnp-org:serviceId:TemperatureSensor1" -and
+            $AdditionalParameters -and $AdditionalParameters.ContainsKey("Variable") -and
+            $AdditionalParameters["Variable"] -eq "CurrentTemperature"
+        }
+
+    }
+}
