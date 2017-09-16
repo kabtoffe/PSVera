eedbe5a7a7339fb863effe74f31c20286f8085cc
Toffe Bergström <toffe@kabtoffe.net>
2017-09-16 19:40:41 +0300

    Switch to standard mock assertion

--- Tests/Invoke-VeraAPI.Tests.ps1
+++ Tests/Invoke-VeraAPI.Tests.ps1
@@ -9,43 +9,61 @@ Describe "Invoke-VeraAPI" {
 
     Connect-Vera -VeraHost "hostname"
 
-    . "$testdir\Mocks\Invoke-Restmethod.ps1"
+    Mock Invoke-RestMethod -ModuleName PSVera
 
     It "No parameters provided" {
         $parameters = Invoke-VeraAPI
-        $parameters["Hostname"] | Should Be "http://hostname:3480/data_request"
-        $parameters["output_format"] | should be "xml"
+        Assert-MockCalled -CommandName Invoke-RestMethod -Scope It -ModuleName PSVera -ParameterFilter {
+            $Uri.ToString().StartsWith("http://hostname:3480/data_request") -and
+            $Uri.ToString().EndsWith("output_format=xml")
+        }
     }
 
     It "ID parameter provided" {
         $parameters = Invoke-VeraAPI -ID "state"
-        $parameters["output_format"] | should be "xml"
-        $parameters["id"] | Should Be "state"
+        Assert-MockCalled -CommandName Invoke-RestMethod -Scope It -ModuleName PSVera -ParameterFilter {
+            $Uri.ToString().StartsWith("http://hostname:3480/data_request") -and
+            $Uri.ToString().Contains("id=state") -and
+            $Uri.ToString().EndsWith("output_format=xml")
+        }
     }
 
     It "ID and action parameter provided" {
         $parameters = Invoke-VeraAPI -ID "state" -Action "rename"
-        $parameters["output_format"] | should be "xml"
-        $parameters["id"] | Should Be "state"
-        $parameters["action"] | Should Be "rename"
+        Assert-MockCalled -CommandName Invoke-RestMethod -Scope It -ModuleName PSVera -ParameterFilter {
+            $Uri.ToString().StartsWith("http://hostname:3480/data_request") -and
+            $Uri.ToString().Contains("id=state") -and
+            $Uri.ToString().Contains("action=rename") -and
+            $Uri.ToString().EndsWith("output_format=xml")
+        }
     }
 
     It "ID, action and devicenum parameter provided" {
         $parameters = Invoke-VeraAPI -ID "state" -Action "rename" -DeviceNum 4
-        $parameters["output_format"] | should be "xml"
-        $parameters["id"] | Should Be "state"
-        $parameters["action"] | Should Be "rename"
-        $parameters["DeviceNum"] | Should Be "4"
+        Assert-MockCalled -CommandName Invoke-RestMethod -Scope It -ModuleName PSVera -ParameterFilter {
+            $Uri.ToString().StartsWith("http://hostname:3480/data_request") -and
+            $Uri.ToString().Contains("id=state") -and
+            $Uri.ToString().Contains("action=rename") -and
+            $Uri.ToString().Contains("DeviceNum=4") -and
+            $Uri.ToString().EndsWith("output_format=xml")
+        }
     }
 
     It "When provided with other output format" {
         $parameters = Invoke-VeraAPI -OutputFormat "json"
-        $parameters["output_format"] | should be "json"
+        Assert-MockCalled -CommandName Invoke-RestMethod -Scope It -ModuleName PSVera -ParameterFilter {
+            $Uri.ToString().StartsWith("http://hostname:3480/data_request") -and
+            $Uri.ToString().EndsWith("output_format=json")
+        }
     }
 
     It "When provided with additional parameters" {
         $parameters = Invoke-VeraAPI -id "action" -AdditionalParameters @{ "foo" = "bar"  }
-        $parameters["id"] | Should Be "action"
-        $parameters["foo"] | Should Be "bar"
+        Assert-MockCalled -CommandName Invoke-RestMethod -Scope It -ModuleName PSVera -ParameterFilter {
+            $Uri.ToString().StartsWith("http://hostname:3480/data_request") -and
+            $Uri.ToString().Contains("?id=action&") -and
+            $Uri.ToString().Contains("foo=bar") -and
+            $Uri.ToString().EndsWith("output_format=xml")
+        }
     }
 }
